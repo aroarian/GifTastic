3636d446c7e5a086835a73c8124b32938e4fad07
aroarian <aroarian0@gmail.com>
2018-07-23 21:37:56 -0400

    fixed download quotes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -128,8 +128,7 @@ $( document ).ready(function() {
 
         var gifImage = $("<img>").addClass("gify");
         
-        var download = $("<a href=" + results[i].images.original.url +
-        "download target='_blank'>" + "<button class='download'>Download</button></a>");
+        var download = $("<a href='" + results[i].images.original.url + "' download target='_blank'>" + "<button class='download'>Download</button></a>");
         
         gifImage
           .attr("src", results[i].images.fixed_height_still.url)
